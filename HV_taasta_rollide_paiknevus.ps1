272fd68da3537605e5310c69e57f642c0a3d2575
seldur <32491935+seldur@users.noreply.github.com>
2024-08-15 21:31:12 +0300

    Update HV_taasta_rollide_paiknevus.ps1
    Added CSV path variable

--- HV_taasta_rollide_paiknevus.ps1
+++ HV_taasta_rollide_paiknevus.ps1
@@ -8,13 +8,14 @@ Käivita see skript mõnel Hyper-V klastri liikmel
 #>
 
 # Fikseeri VM'ide paiknevus enne tegevusi
-Get-ClusterGroup | where GroupType -eq "VirtualMachine" | select Name,OwnerNode | Export-Csv -NoTypeInformation -Path C:\Temp\test\1.txt
+$CSVpath = 'C:\Temp\1.txt'
+Get-ClusterGroup | where GroupType -eq "VirtualMachine" | select Name,OwnerNode | Export-Csv -NoTypeInformation -Path $CSVpath
 
 Write-Host "Tegevuste aeg (---või poolita skript siit, et vältida fikseeritud seisu üle kirjutamine---)"
 pause
 
 # Taasta VM'ide paiknevus, mis oli enne tegevusi
-$VMs = Import-Csv C:\Temp\test\1.txt
+$VMs = Import-Csv $CSVpath
 
 foreach ($VM in $VMs) {
     Move-ClusterVirtualMachineRole -Name (Get-ClusterGroup -Name $VM.Name) -Node (Get-ClusterNode $VM.OwnerNode) -WarningAction Ignore -ErrorAction Ignore
